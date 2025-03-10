c59a0c02df605662d5114acfc6cf0709fd46194a
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2025-03-10 18:23:17 -0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -8,4 +9,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	}
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog Cheche';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cores da Natureza';
+export const SITE_DESCRIPTION = 'Um espaço para compartilhar práticas e reflexões que trazem equilíbrio, leveza e bem-estar à vida. Pequenos passos em direção à saúde e sustentabilidade para um mundo melhor.';
